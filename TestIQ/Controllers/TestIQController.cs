58c5e1ac70928001e77ab3e7e0ef8d5ff0a24dde
Cuong Truong Quoc <truongcuong05971@hotmail.com>
2018-03-24 22:56:26 +0700

    Add project files.

---
+++ TestIQ/Controllers/TestIQController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestIQ.Controllers
+{
+    public class TestIQController : Controller
+    {
+        // GET: TestIQ
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult IQNguoinoitieng()
+        {
+            return View();
+        }
+    }
+}
